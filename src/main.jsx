8d66703d927930d8d0390111e07540622ccb35b2
Leyla Bonilla <leylama75@gmail.com>
2022-01-20 20:04:39 +0000

    checkbox working (this app it's without babel)

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,44 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+class Todo extends React.Component {
+  constructor(props){
+      super(props);
+
+      // set done attribute and text attribute pass down
+      this.state = { done: (this.props.done == "true" && props.done),
+                     text: props.text
+                   };
+
+      // click event call handleClick function of this class
+      this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    // set checked box to the opposite of the current state (done)
+    this.setState(
+      state => ({
+        done: !state.done
+      })
+    );
+  }
+
+  render() {
+    // drawing a div with class todo and a checkbox inside with
+    // the text attribute pass as the text
+    // se inicializan en el constructor arriba
+    return <div className="todo">
+              <span>
+                  <input type="checkbox" checked={this.state.done} onClick={this.handleClick} />
+                  <input type="text" value={this.state.text} />
+                </span>
+           </div>;
+  }
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Todo text="Todo one" done="false" />
   </React.StrictMode>,
   document.getElementById('root')
 )
